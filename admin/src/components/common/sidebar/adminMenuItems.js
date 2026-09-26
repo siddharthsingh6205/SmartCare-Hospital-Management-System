e4400d78cc2c36a61354f0a40b65a1b81f238fea
siddharthsingh <siddharthsingh620@gmail.com>
2026-09-27 00:04:54 +0530

    Add Dockerfiles for deployment

--- admin/src/components/common/sidebar/adminMenuItems.js
+++
@@ -1,8 +0,0 @@
-import { assets } from "../../../assets/assets";
-
-export   const adminMenuItems = [
-    { to: "/admin-dashboard", icon: assets.home_icon, text: "Dashboard" },
-    { to: "/all-appointments", icon: assets.appointment_icon, text: "Appointments" },
-    { to: "/add-doctor", icon: assets.add_icon, text: "Add Doctor" },
-    { to: "/doctor-list", icon: assets.people_icon, text: "Doctors List" },
-  ];
--- admin/src/components/common/sidebar/doctorMenuItems.js
+++
@@ -1,8 +0,0 @@
-import { assets } from "../../../assets/assets";
-
-  // Doctor menu items
-  export const doctorMenuItems = [
-    { to: "/doctor-dashboard", icon: assets.home_icon, text: "Dashboard" },
-    { to: "/doctor-appointments", icon: assets.appointment_icon, text: "Appointments" },
-    { to: "/doctor-profile", icon: assets.people_icon, text: "Profile" }
-  ];
